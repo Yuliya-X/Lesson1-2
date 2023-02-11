ce409558885afa393bcad2dc0ee2b1f3f516ce3c
Yuliya <darkwader@mail.ru>
2023-02-11 20:39:16 +0300

    N_Yes_No

---
+++ N_Yes_No/Program.cs
@@ -0,0 +1,35 @@
+﻿double N;
+
+N = 4;
+
+if (N % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+N = -3;
+
+if (N % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+N = 7;
+
+
+if (N % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
