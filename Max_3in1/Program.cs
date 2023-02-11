121b883d9ff99c6e24fb9a339ce4d161bc46b4f3
Yuliya <darkwader@mail.ru>
2023-02-11 19:58:44 +0300

    Max_3in1

---
+++ Max_3in1/Program.cs
@@ -0,0 +1,86 @@
+﻿double a, b, c;
+double max;
+
+a = 2;
+b = 3;
+c = 7;
+
+if (a < b)
+{
+    if (b < c)
+    {
+        max = c;
+    }
+else
+    {
+    max = b;
+    }
+}
+else
+{
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+Console.WriteLine(max);
+
+a = 44;
+b = 5;
+c = 78;
+
+if (a < b)
+{
+    if (b < c)
+    {
+        max = c;
+    }
+else
+    {
+    max = b;
+    }
+}
+else
+{
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+Console.WriteLine(max);
+
+a = 22;
+b = 3;
+c = 9;
+
+if (a < b)
+{
+    if (b < c)
+    {
+        max = c;
+    }
+else
+    {
+    max = b;
+    }
+}
+else
+{
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+Console.WriteLine(max);
