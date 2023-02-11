e8085f5cae9235d8e88dc4c7a53f86ae4616ce62
Yuliya <darkwader@mail.ru>
2023-02-11 18:41:45 +0300

    MaxMin

---
+++ MaxMin/Program.cs
@@ -0,0 +1,62 @@
+﻿double a, b;
+double max, min;
+
+a = 5;
+b = 7;
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+else
+    {
+    max = a;
+    min = b;
+    }
+
+Console.Write("max= ");
+Console.Write(max);
+
+Console.Write("min= ");
+Console.Write(min);
+
+a = 2; 
+b = 10; 
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+else
+    {
+    max = a;
+    min = b;
+    }
+
+Console.Write("max= ");
+Console.Write(max);
+
+Console.Write("min= ");
+Console.Write(min);
+
+a = -9; 
+b = -3; 
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+else
+    {
+    max = a;
+    min = b;
+    }
+
+Console.Write("max= ");
+Console.Write(max);
+
+Console.Write("min= ");
+Console.Write(min);
