10cc6a002e3f301e922a05bba2f64417047fa2d0
Yuliya <darkwader@mail.ru>
2023-02-12 11:55:33 +0300

    from1toN

---
+++ from1toN/Program.cs
@@ -0,0 +1,24 @@
+﻿double n;
+n = 5;
+double index;
+index = 0;
+
+Console.WriteLine("Чётные от первого значения: ");
+
+while(index < n - 1)
+{
+    index = index + 2;
+    Console.WriteLine(index);
+}
+
+
+Console.WriteLine("Чётные от второго значения: ");
+
+n = 8;
+index = 0;
+
+while(index < n - 1)
+{
+    index = index + 2;
+    Console.WriteLine(index);
+}
